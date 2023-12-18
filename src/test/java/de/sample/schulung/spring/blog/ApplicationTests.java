dbe71381e25536fcd6d8050880b97fc04b9618af
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-12-18 10:54:23 +0100

    Initial commit

---
+++ src/test/java/de/sample/schulung/spring/blog/ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sample.schulung.spring.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
