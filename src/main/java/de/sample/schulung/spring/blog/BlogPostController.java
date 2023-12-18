081189700831e9b75a2cd11619bbc09f65789a0a
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-12-18 14:22:45 +0100

    add GET /blogposts -> 200

---
+++ src/main/java/de/sample/schulung/spring/blog/BlogPostController.java
@@ -0,0 +1,18 @@
+package de.sample.schulung.spring.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/blogposts")
+public class BlogPostController {
+
+    @GetMapping
+    @ResponseBody
+    String sayHello() {
+        return "Hello World";
+    }
+
+}
---
+++ src/test/java/de/sample/schulung/spring/blog/BlogPostApiTests.java
@@ -0,0 +1,31 @@
+package de.sample.schulung.spring.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class BlogPostApiTests {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void shouldReturn200WhenGetBlogPosts() throws Exception {
+        mvc.perform(
+                        get("/blogposts")
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(
+                        status().isOk()
+                );
+    }
+
+}
