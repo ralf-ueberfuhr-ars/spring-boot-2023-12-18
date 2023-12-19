eb1460377e501a92e32495bced0e791cfd6eda9f
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-12-19 11:33:54 +0100

    add further tests

---
+++ src/test/java/de/sample/schulung/spring/blog/BlogPostControllerIntegrationTests.java
@@ -0,0 +1,33 @@
+package de.sample.schulung.spring.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// SO NICHT!
+@SpringBootTest
+public class BlogPostControllerIntegrationTests {
+
+  @Autowired
+  BlogPostController controller;
+
+  @Test
+  void shouldCreateBlogPostSuccessfully() {
+    final var blogPost = new BlogPostDto();
+    blogPost.setTitle("test");
+    blogPost.setContent("Das ist ein Test");
+
+    final var result = controller.createBlogPost(blogPost);
+
+    assertThat(result.getStatusCode().value())
+      .isEqualTo(HttpStatus.CREATED.value());
+    assertThat(result.getBody())
+      .isNotNull();
+    assertThat(result.getBody().getTitle())
+      .isEqualTo(blogPost.getTitle());
+
+  }
+}
---
+++ src/test/java/de/sample/schulung/spring/blog/BlogPostControllerTests.java
@@ -0,0 +1,28 @@
+package de.sample.schulung.spring.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BlogPostControllerTests {
+
+  @Test
+  void shouldCreateBlogPostSuccessfully() {
+    final var controller = new BlogPostController();
+    final var blogPost = new BlogPostDto();
+    blogPost.setTitle("test");
+    blogPost.setContent("Das ist ein Test");
+
+    final var result = controller.createBlogPost(blogPost);
+
+    assertThat(result.getStatusCode().value())
+      .isEqualTo(HttpStatus.CREATED.value());
+    assertThat(result.getBody())
+      .isNotNull();
+    assertThat(result.getBody().getTitle())
+      .isEqualTo(blogPost.getTitle());
+
+  }
+
+}
