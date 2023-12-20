471b34ffa70d88fad9d710fa9e3c6c4216d1d69d
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-12-20 11:35:01 +0100

    add validation in boundary

--- src/main/java/de/sample/schulung/spring/blog/boundary/BlogPostDto.java
+++ src/main/java/de/sample/schulung/spring/blog/boundary/BlogPostDto.java
@@ -1,6 +1,8 @@
 package de.sample.schulung.spring.blog.boundary;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -11,7 +13,11 @@ public class BlogPostDto {
 
   @JsonProperty(access = JsonProperty.Access.READ_ONLY)
   private UUID id;
+  @NotNull
+  @Size(min = 3)
   private String title;
+  @NotNull
+  @Size(min = 10)
   private String content;
   @JsonProperty(access = JsonProperty.Access.READ_ONLY)
   private LocalDateTime timestamp;
--- src/main/java/de/sample/schulung/spring/blog/boundary/GlobalExceptionHandler.java
+++ src/main/java/de/sample/schulung/spring/blog/boundary/GlobalExceptionHandler.java
@@ -1,13 +1,21 @@
 package de.sample.schulung.spring.blog.boundary;
 
 import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@SuppressWarnings("NullableProblems")
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler(ConstraintViolationException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -16,4 +24,25 @@ public class GlobalExceptionHandler {
     // return ResponseEntity, Fehler-Object (JSON), ProblemDetail
   }
 
+  @Override
+  protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
+    HttpMediaTypeNotSupportedException ex,
+    HttpHeaders headers,
+    HttpStatusCode status,
+    WebRequest request
+  ) {
+    // eigenes Fehlerhandling
+    return super.handleHttpMediaTypeNotSupported(ex, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+    MethodArgumentNotValidException ex,
+    HttpHeaders headers,
+    HttpStatusCode status,
+    WebRequest request
+  ) {
+    // eigenes Fehlerhandling
+    return super.handleMethodArgumentNotValid(ex, headers, status, request);
+  }
 }
